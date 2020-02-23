5a8424cef78c1b3014601e8e21229206dd058758
samcraigdev <contact@samcraig.io>
2020-02-22 19:55:37 -0500

    ✨ added MustMapEnv

--- osx/env.go
+++ osx/env.go
@@ -1,6 +1,9 @@
 package osx
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 // GetenvDefault returns an environment variable or the default value if it is empty.
 func GetenvDefault(key string, def string) string {
@@ -8,4 +11,13 @@ func GetenvDefault(key string, def string) string {
 		return value
 	}
 	return def
+}
+
+// MustMapEnv maps the provided environment varible to a key in the provided struct.
+func MustMapEnv(target *string, def string) {
+	var value string
+	if value = os.Getenv(def); value == "" {
+		fmt.Print(fmt.Sprintf("environment variable %q not set", def))
+	}
+	*target = value
 }
